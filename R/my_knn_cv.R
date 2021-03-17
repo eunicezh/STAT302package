48c7b745d69ab781e31dd8f29e381fd68c13f865
Eunice Zhang <eunicezh99@outlook.com>
2021-03-17 09:29:38 -0700

    revise

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -12,7 +12,8 @@
 #' @keywords \code{prediction}
 #'
 #' @examples
-#' my_knn_cv(5, penguins[,3:6],penguins[[1]], 1)
+#' train <- na.omit(my_penguins)
+#' my_knn_cv(5, train[,3:6],train[[1]], 1)
 #'
 #' @export
 my_knn_cv <- function(k_cv, train, cl, k_nn) {
@@ -22,17 +23,20 @@ my_knn_cv <- function(k_cv, train, cl, k_nn) {
   # make an empty list that stores the misclassfication error for each fold
   error_list = rep(NA, k_cv)
   # split training data and test data based on i
-  for (i in 1:k_cv) {
-    data_train <- data %>% filter(split != i)
-    data_test <- data %>% filter(split == i)
-    cl_test <- as.numeric(data_test$y)
-    pred <- knn(data_train[,1:4], data_test[,1:4], as.factor(data_train$y), k=k_nn)
-    # record the misclassfication rate for each fold
-    error_list[i] <- mean(data_test$y != pred)
+  if (require("dplyr","class")) {
+    for (i in 1:k_cv) {
+      data_train <- data %>% filter(split != i)
+      data_test <- data %>% filter(split == i)
+      cl_test <- as.numeric(data_test$y)
+      pred <- knn(data_train[,1:4], data_test[,1:4], as.factor(data_train$y), k=k_nn)
+      # record the misclassfication rate for each fold
+      error_list[i] <- mean(data_test$y != pred)
+    }
+    # record the average of all misclassfication rate
+    cv_err <- mean(error_list)
+    # predict the class by using the full data
+    class <- knn(train, train, as.factor(data$y), k=k_nn)
+    return(list("class"= class,"cv_err" = cv_err))
   }
-  # record the average of all misclassfication rate
-  cv_err <- mean(error_list)
-  # predict the class by using the full data
-  class <- knn(train, train, as.factor(data$y), k=k_nn)
-  return(list("class"= class,"cv_err" = cv_err))
 }
+
