b3bafb9959e3232aab42fe19b2fe0a2392055372
Eunice Zhang <eunicezh99@outlook.com>
2021-03-17 20:47:41 -0700

    new change

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -9,3 +9,4 @@ test_that("my_knn_cv returns a numeric Estimate", {
 test_that("inappropriate inputs throw error", {
   expect_error(my_knn_cv(5, my_penguins[,3:6],my_penguins[[1]], "a string" ))
 })
+
