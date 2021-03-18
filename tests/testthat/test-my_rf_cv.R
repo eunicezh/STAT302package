dd4265cc57cbc2170fcd904398fb63d4f2d69b9a
Eunice Zhang <eunicezh99@outlook.com>
2021-03-17 21:05:12 -0700

    add test for random forest

---
+++ tests/testthat/test-my_rf_cv.R
@@ -0,0 +1,9 @@
+
+
+test_that("my_rf_cv returns a numeric Estimate", {
+  expect_type(my_rf_cv(5),"double")
+})
+
+test_that("inappropriate input throw error", {
+  expect_error(my_rf_cv("a string"))
+})
